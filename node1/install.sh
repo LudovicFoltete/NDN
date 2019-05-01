68ad7ea51e9bd61c44cff40adb4c29b5fd212772
LudovicFoltete <ludovicflb@gmail.com>
2019-05-01 21:46:30 +0200

    update for 2 nodes

--- node1/install.sh
+++ node1/install.sh
@@ -1,13 +1,13 @@
 #SETTING UP THE SECURITY
 
 #generate the router key
-ndnsec-key-gen /ndn/upec/%C1.Router/client > client.key
+ndnsec-key-gen /ndn/upec/%C1.Router/router1 > router1.key
 
 #generate the certificate for the router key
-ndnsec-cert-dump -i /ndn/upec/%C1.Router/client > client.cert
+ndnsec-cert-dump -i /ndn/upec/%C1.Router/router1 > router1.cert
 
 #install the router certificate
-ndnsec-cert-install -f client.cert
+ndnsec-cert-install -f router1.cert
 
 #to verify that the certificates have been installed
 ndnsec-list
@@ -21,7 +21,7 @@ sudo mv interfaces /etc/network/
 nohup nfd-start
 
 #configure each face that a computer uses to connect to a neighboring computer
-nfdc face create udp4://192.168.10.1
+nfdc face create udp4://192.168.10.254
 nfdc face create udp4://192.168.10.2
 
 #SETTING UP THE CONFIGURATION FILE
@@ -29,7 +29,7 @@ nfdc face create udp4://192.168.10.2
 #move all files in the NLSR directory
 mv ~/NLSR/nlsr.conf ~/NLSR/nlsr.conf.backup
 mv nlsr.conf ~/NLSR/
-mv client.cert ~/NLSR/
-mv client.key ~/NLSR/
+mv router1.cert ~/NLSR/
+mv router1.key ~/NLSR/
 mv start_nlsr.sh ~/NLSR/
 
--- node2/install.sh
+++ node2/install.sh
@@ -1,13 +1,13 @@
 #SETTING UP THE SECURITY
 
 #generate the router key
-ndnsec-key-gen /ndn/upec/%C1.Router/client > client.key
+ndnsec-key-gen /ndn/upec/%C1.Router/router2 > router2.key
 
 #generate the certificate for the router key
-ndnsec-cert-dump -i /ndn/upec/%C1.Router/client > client.cert
+ndnsec-cert-dump -i /ndn/upec/%C1.Router/router2 > router2.cert
 
 #install the router certificate
-ndnsec-cert-install -f client.cert
+ndnsec-cert-install -f router2.cert
 
 #to verify that the certificates have been installed
 ndnsec-list
@@ -29,7 +29,7 @@ nfdc face create udp4://192.168.10.2
 #move all files in the NLSR directory
 mv ~/NLSR/nlsr.conf ~/NLSR/nlsr.conf.backup
 mv nlsr.conf ~/NLSR/
-mv client.cert ~/NLSR/
-mv client.key ~/NLSR/
+mv router2.cert ~/NLSR/
+mv router2.key ~/NLSR/
 mv start_nlsr.sh ~/NLSR/
 

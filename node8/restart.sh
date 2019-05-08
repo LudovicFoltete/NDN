752d9831391054a4beca5a472682a1ddc366a3d2
LudovicFoltete <ludovicflb@gmail.com>
2019-05-08 22:31:02 +0200

    Adding of a 'restart' script after a shutting down

---
+++ node8/restart.sh
@@ -0,0 +1,24 @@
+#start nfd
+nohup nfd-start > /dev/null
+
+#wait a little to let time for nfd to start completely
+echo "NFD IS STARTING, PLEASE WAIT..."
+sleep 15
+
+#configure each face that a computer uses to connect to a neighboring computer
+nfdc face create udp4://192.168.10.1
+nfdc face create udp4://192.168.10.2
+nfdc face create udp4://192.168.10.3
+nfdc face create udp4://192.168.10.4
+nfdc face create udp4://192.168.10.5
+nfdc face create udp4://192.168.10.6
+nfdc face create udp4://192.168.10.7
+nfdc face create udp4://192.168.10.254
+nfdc face create udp4://192.168.10.9
+nfdc face create udp4://192.168.10.10
+nfdc face create udp4://192.168.10.11
+nfdc face create udp4://192.168.10.12
+nfdc face create udp4://192.168.10.13
+nfdc face create udp4://192.168.10.14
+nfdc face create udp4://192.168.10.15
+nfdc face create udp4://192.168.10.16
